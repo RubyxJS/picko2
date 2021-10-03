5d04ce8c0d6a8c332be96b45c99f93ba46964664
Reuben <68828483+RubyxJS@users.noreply.github.com>
2021-10-03 14:00:56 +0100

    creates canvas

--- MyCanvas.java
+++ MyCanvas.java
@@ -1,30 +1,17 @@
 import java.awt.*;
-
 // class to construct a frame and containing main method    
-class Wheel {
+class MyCanvas {
     // class constructor
-    public Wheel(int frameWidth, int frameHeight) {
+    public MyCanvas(int frameWidth, int frameHeight) {
 
         // creating a frame
         Frame f = new Frame("Picko");
         // adding canvas to frame
-        f.add(new MyCanvas());
+        f.add(new Wheel());
 
         // setting layout, size and visibility of frame
         f.setLayout(null);
         f.setSize(frameWidth, frameHeight);
         f.setVisible(true);
     }
-    // class which inherits the Canvas class
-// to create Canvas
-
-
-    // main method
-    public static void main(String[] args) {
-        int frameWidth = 1000;
-        int frameHeight = 1000;
-        new Wheel(frameWidth, frameHeight);
-    }
 }
-
-
---
+++ Wheel.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+    // class which inherits the Canvas class
+    // to create Canvas
+    class Wheel extends Canvas {
+        // class constructor
+        public Wheel() {
+            setBackground(Color.GRAY);
+            setSize(1000, 1000);
+        }
+
+    }
